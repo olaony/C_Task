26a3f3ced6c6a428d5dc600cdfff07ce315e0890
olga <kudriakova.olga@gmail.com>
2022-10-11 23:59:05 +0300

    Print new array

--- Program.cs
+++ Program.cs
@@ -16,3 +16,12 @@ for (int i = 0; i < array.Length; i++)
         count++;
         }
     }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arrayRes);
